55bddf4c3f9fa1311abc7820d8c3ea3038e31e28
huggoparcelly <h.parcelly@gmail.com>
2024-01-29 19:21:53 -0300

    add parameterized test

--- tests/parametrized_test.py
+++ tests/parametrized_test.py
@@ -4,3 +4,18 @@ from src.hex_converter import hexadecimal_to_decimal  # noqa: F401
 
 # aplica o marcador de dependency para todos os testes do arquivo
 pytestmark = pytest.mark.dependency  # NÃO REMOVA ESSA LINHA
+
+@pytest.mark.parametrize(
+        "input_hexa, expected_dec",
+        [
+            ("8", 8),
+            ("9", 9),
+            ("a", 10),
+            ("b", 11),
+            ("c", 12),
+            ("e", 14),
+            ("f", 15),
+        ]
+)
+def test_converter(input_hexa, expected_dec):
+    assert hexadecimal_to_decimal(input_hexa) == expected_dec
